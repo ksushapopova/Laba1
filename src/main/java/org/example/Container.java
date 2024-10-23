f033b262c78e5ec34d4cb6e39e1942447b186472
ksushapopova <ks.popova.2005@mail.ru>
2024-10-23 22:42:45 +0300

    Реализованы добавление и удаление элементов из контейнера, вывод и get

--- src/main/java/org/example/Container.java
+++ src/main/java/org/example/Container.java
@@ -20,6 +20,36 @@ public class Container{
         return size == 0;
     }
 
+    public void add(int item) {
+        ensureCapacity();
+        items[size++] = item;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return items[index];
+    }
+
+    public boolean remove(int item) {
+        for (int i = 0; i < size; i++) {
+            if (items[i] == item) {
+                // Сдвигаем элементы влево
+                System.arraycopy(items, i + 1, items, i, size - i - 1);
+                size--;
+                return true;
+            }
+        }
+        return false; // Элемент не найден
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(items[i]);
+        }
+    }
+
     private void ensureCapacity() {
         if (size >= items.length) {
             int newCapacity = items.length * 2;
